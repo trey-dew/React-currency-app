9fc86a821e4e191f56936cc6759079ed293484af
coolcatmeow <trey.dew@outlook.com>
2023-06-02 21:13:14 -0500

    hi

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, {useState, useRef, useEffect} from 'react'
+import TodoList from './TodoList'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function uniqueIds(){
+    return Math.random(10000)
+  }
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+      const name = todoNameRef.current.value
+      if(name === '') return
+      setTodos(prevTodos =>{
+        return [...prevTodos, {id: uniqueIds(), name: name, complete: false}]
+      })
+      console.log(name)
+    }
+
+    function handleClearTodos(){
+      const newTodos = todos.filter(todo => !todo.complete)
+      setTodos(newTodos)
+    }
+
+    return (
+      <>
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
+        <input ref={todoNameRef} type="text" />
+        <button onClick={handleAddTodo}>Add</button>
+        <button onClick={handleClearTodos}>Clear</button>
+        <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+      </>
+    )
+  
+}
+
+export default App;
